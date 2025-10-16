b127b859d4a7e2fe6efd8d3b30e3ce55fba12cbc
jeepinit77 <68778774+jeepinit77@users.noreply.github.com>
2025-10-15 22:26:50 -0500

    Expand sample activities to cover all default types

--- app/src/main/java/com/jumblemint/cows/data/model/ActivityType.kt
+++ app/src/main/java/com/jumblemint/cows/data/model/ActivityType.kt
@@ -7,6 +7,15 @@ enum class ActivityType(val displayName: String) {
     DECEASED("Deceased"),
     WORKED("Worked"),
     CASTRATED("Castrated"),
+    BRED("Bred"),
+    CALVED("Calved"),
+    VACCINATED("Vaccinated"),
+    TREATED("Treated"),
+    WEIGHED("Weighed"),
+    PURCHASED("Purchased"),
+    HEALTH_CHECK("Health Check"),
+    TAGGED("Tagged"),
+    NOTE("Note"),
     BIRTH("Birth"),
     OTHER("Other")
 }
