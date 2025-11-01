958ee41839628c5e1552b04fa12794d9fba33274
jeepinit77 <68778774+jeepinit77@users.noreply.github.com>
2025-10-31 20:30:13 -0500

    Fix KeyboardOptions import on quick add screen

--- app/src/main/java/com/jumblemint/cows/ui/screens/cows/QuickAddCattleScreen.kt
+++ app/src/main/java/com/jumblemint/cows/ui/screens/cows/QuickAddCattleScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.ExpandLess
@@ -50,7 +51,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
